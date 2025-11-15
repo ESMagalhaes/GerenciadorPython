24de8fc1a615b29785d2f7fab9c35edf381b18d8
ESMagalhaes <es.magalhaes24@gmail.com>
2025-11-15 16:27:31 -0300

    :recycle: refactor: Alteração da seção connection.py para se alinhar com a estrutura da config.py

--- app/database/connection.py
+++ app/database/connection.py
@@ -1,28 +1,22 @@
 # Importando os módulos necessários
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-from dotenv import load_dotenv
-import os
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from app.core.config import settings
 
-load_dotenv()  # Carrega as variáveis de ambiente do arquivo .env
+# Criando o engine do banco de dados
+engine = create_engine(settings.db_url, echo=True)
 
-db_url = os.getenv("DATABASE_URL")
-
-# Configuração da conexão com o banco de dados
-engine = create_engine(
-    db_url,
-    pool_pre_ping=True
-)
-
-# Criação da classe base para os modelos
-Base = declarative_base()
+# Base declarativa para os modelos
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
     bind=engine
 )
 
-# Função para obter uma sessão do banco de dados
+# Classe base para os modelos do banco de dados
+class Base(DeclarativeBase):
+    pass
+
 def get_db():
     db = SessionLocal()
     try:
